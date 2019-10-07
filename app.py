51a0a7c31663c9452e968f639586db7e8138f077
Josiah Argo <josiah-argo@uiowa.edu>
2019-10-07 13:03:06 -0500

    modified to include basic Jinja template

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
 
 @app.route('/josiah')
 def josiah():
-    return "hello Josiah!"
+    return render_template('josiah.html', pageTitle='About Josiah')
 
 if __name__ == '__main__':
     app.run(debug=True)
