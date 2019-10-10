22f57db2dbca3574a51a672c16256e5a6a80347d
Josiah Argo <josiah-argo@uiowa.edu>
2019-10-10 14:47:05 -0500

    index.html and app.py working, ready to merge and deploy

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', display="", pageTitle='Loan Calculator')
 
-
-@app.route('/josiah')
-def josiah():
-    return render_template('josiah.html', pageTitle='About Josiah')
+@app.route('/loan', methods=['GET', 'POST'])
+def loan():
+    if request.method == 'POST':
+        form = request.form
+        A = float(form['numOne'])
+        numPmt = float(form['numTwo'])
+        numYears = float(form['numThree'])
+        intRate = float(form['numFour'])
+        n = numPmt * numYears
+        i = (intRate / 100) / numPmt
+        D = ((( 1 + i )**n ) - 1 ) / ( i * ( 1 + i)**n )
+        calc = A / D
+        calc = (round(calc, 2))
+        return render_template('index.html', display=calc, pageTitle='Loan Calculator')
+   
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
